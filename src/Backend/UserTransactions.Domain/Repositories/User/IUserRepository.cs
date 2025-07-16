0a5ace6a736a2cb64414f96ff68f8afdf7fae21a
Flaviojcf <flaviojcostafilho@gmail.com>
2025-07-15 23:43:06 -0300

    test

--- src/Backend/UserTransactions.Domain/Repositories/User/IUserRepository.cs
+++ src/Backend/UserTransactions.Domain/Repositories/User/IUserRepository.cs
@@ -4,6 +4,6 @@ namespace UserTransactions.Domain.Repositories.User
 {
     public interface IUserRepository
     {
-        Task AddAsync(UserEntity user);
+        Task AddAsync(UserEntity user);;
     }
 }
