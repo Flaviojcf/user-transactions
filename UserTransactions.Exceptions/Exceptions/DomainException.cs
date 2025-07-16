fdec23b75dcdaf2a4ae88c962989734ec3ddefeb
Flaviojcf <flaviojcostafilho@gmail.com>
2025-07-15 23:38:07 -0300

    refactor(exceptions): remove DomainException and restructure project
    Eliminate DomainException and UserTransactionsException classes.
    Restructure UserTransactions.Exceptions.csproj and update project paths
    for UserTransactions.Tests to reflect new directory structure.

--- UserTransactions.Exceptions/Exceptions/DomainException.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace UserTransactions.Exceptions.Exceptions
-{
-    public class DomainException : UserTransactionsException
-    {
-        public DomainException(string message) : base(message) { }
-    }
-}
