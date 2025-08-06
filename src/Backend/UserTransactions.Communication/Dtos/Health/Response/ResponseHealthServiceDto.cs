d64e1a8a6e17ef0b6682389c22f88fbc4aa06a30
Flaviojcf <flaviojcostafilho@gmail.com>
2025-08-06 10:48:52 -0300

    refactor: Remove folder

--- src/Backend/UserTransactions.Communication/Dtos/Health/Response/ResponseHealthServiceDto.cs
+++
@@ -1,8 +0,0 @@
-namespace UserTransactions.Communication.Dtos.Health.Response
-{
-    public class ResponseHealthServiceDto
-    {
-        public string Service { get; set; } = string.Empty;
-        public string Status { get; set; } = string.Empty;
-    }
-}
